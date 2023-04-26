a239b84b008b26f3e830444b7181b7295a4f74c1
Tejas Vivek Palyekar <tejaspalyekar18@gmail.com>
2023-04-26 12:18:10 +0530

    commmit

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -11,27 +11,34 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _Questions extends State<QuestionScreen> {
-  final currentquestions = questions[2];
+  final currentquestions = questions[3];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentquestions.text,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentquestions.answers.map((answer) {
-            return AnswerButton(answer: answer, onTap: () {});
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentquestions.text,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentquestions.answers.map((answer) {
+              return AnswerButton(answer: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
